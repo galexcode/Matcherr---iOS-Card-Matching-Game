feac296a0bd07d8d0c9e5104ca65f7514dd33166
Aaron Rama <aaronrama@gmail.com>
2013-10-15 22:44:34 -0700

    Add initial CardMatchingGame model

---
+++ Matcherr/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matcherr
+//
+//  Created by Aaron Rama on 10/15/13.
+//  Copyright (c) 2013 Aaron Rama. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matcherr/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matcherr
+//
+//  Created by Aaron Rama on 10/15/13.
+//  Copyright (c) 2013 Aaron Rama. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
