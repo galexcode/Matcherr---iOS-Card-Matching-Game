07227c9ad0f57f78637524418f8af8e117db6ed0
Aaron Rama <aaronrama@gmail.com>
2013-10-15 21:41:46 -0700

    Add Main View and Controller

--- Matcherr/CardGameViewController.m
+++ Matcherr/CardGameViewController.m
@@ -9,21 +9,24 @@
 #import "CardGameViewController.h"
 
 @interface CardGameViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
+@property (nonatomic) int flipCount;
 @end
 
 @implementation CardGameViewController
 
-- (void)viewDidLoad
+- (IBAction)flipCard:(UIButton *)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    sender.selected = !sender.isSelected;
+    self.flipCount++;
 }
 
-- (void)didReceiveMemoryWarning
+-(void)setFlipCount:(int)flipCount
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _flipCount = flipCount;
+    
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
 }
+
 
 @end
